d312df765f28c21bc2640587846fd78266cae241
CeeDeeBee <cdbarnes98@gmail.com>
2020-04-14 15:43:11 -0400

    Initialized with db

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const carsRouter = require("../cars/carsRouter");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/cars", carsRouter);
+
+server.get("/", (req, res) => {
+	res.send("Server Running");
+});
+
+module.exports = server;
