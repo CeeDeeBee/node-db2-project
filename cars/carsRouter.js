9630cf3ab118dc79fc0ae8c860c5073922603e86
CeeDeeBee <cdbarnes98@gmail.com>
2020-04-15 01:21:51 -0400

    Added UPDATE and DELETE routes

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -16,17 +16,8 @@ router.get("/", (req, res) => {
 		});
 });
 
-router.get("/:id", (req, res) => {
-	const { id } = req.params;
-
-	db("cars")
-		.where({ id })
-		.first()
-		.then((car) => res.status(200).json(car))
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err.message });
-		});
+router.get("/:id", validateCarID, (req, res) => {
+	res.status(200).json(req.car);
 });
 
 router.post("/", (req, res) => {
@@ -49,5 +40,62 @@ router.post("/", (req, res) => {
 			.json({ message: "Car must have VIN, make, model, and mileage." });
 	}
 });
+
+router.put("/:id", validateCarID, (req, res) => {
+	const { id } = req.params;
+
+	db("cars")
+		.where({ id })
+		.update(req.body)
+		.then(() => {
+			db("cars")
+				.where({ id })
+				.first()
+				.then((car) => res.status(201).json(car))
+				.catch((err) => {
+					console.log(err);
+					res.status(500).json({ error: err.message });
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err.message });
+		});
+});
+
+router.delete("/:id", validateCarID, (req, res) => {
+	const { id } = req.params;
+
+	db("cars")
+		.where({ id })
+		.del()
+		.then((count) =>
+			res.status(200).json({ message: `${count} record(s) deleted` })
+		)
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err.message });
+		});
+});
+
+function validateCarID(req, res, next) {
+	const { id } = req.params;
+
+	db("cars")
+		.where({ id })
+		.first()
+		.then((car) => {
+			if (car) {
+				req.car = car;
+				next();
+			} else {
+				res.status(404).json({ message: "Car Not Found" });
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err.message });
+		});
+}
 
 module.exports = router;
